dd771d8c198988185b2c9b2ce3ee87b96c2ef1a7
fernandojoseropero <sdjd7yab4ph4zlsuho5f6j5t4zzagm2iswf72bress3mclqatbgq>
2021-11-19 00:41:50 -0500

    feat: adding loaders to services

--- src/commons/Buttons/ButtonServices/index.js
+++ src/commons/Buttons/ButtonServices/index.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import {TouchableOpacity, View} from 'react-native';
+import {ActivityIndicator, TouchableOpacity, View} from 'react-native';
 import {styles} from './styles';
 import MyText from '../../MyText';
 
-export default function ButtonServices({service, icon, color}) {
+export default function ButtonServices({
+  handlerAction,
+  loading,
+  service,
+  icon,
+  color,
+}) {
   return (
-    <TouchableOpacity style={styles.container}>
+    <TouchableOpacity
+      disabled={loading}
+      onPress={handlerAction}
+      style={styles.container}>
       <View style={styles.containerIcon}>
         <Icon name={icon} color={color} size={20} />
       </View>
-      <MyText customStyles={styles.text}>Continue with {service}</MyText>
+      {loading ? (
+        <ActivityIndicator />
+      ) : (
+        <MyText customStyles={styles.text}>Continue with {service}</MyText>
+      )}
     </TouchableOpacity>
   );
 }
--- src/screens/Signin/index.js
+++ src/screens/Signin/index.js
@@ -2,13 +2,18 @@ import React, {useState} from 'react';
 import {styles} from './styles';
 import Wrapper from '../../components/WrapperMain';
 import MyText from '../../commons/MyText';
-import {Alert, View} from 'react-native';
+import {Alert, View, Platform} from 'react-native';
 import {SinginForm} from '../../components/Auth';
 import {ButtonRounded, ButtonServices} from '../../commons/Buttons';
 import colors from '../../themes/colors';
 
 export default function Signin() {
   const [loading, setLoading] = useState(false);
+  const [loadingService, setLoadingService] = useState({
+    apple: false,
+    facebook: false,
+    google: false,
+  });
   const [formValues, setFormValues] = useState({
     userName: '',
     password: '',
@@ -28,6 +33,12 @@ export default function Signin() {
     }
     setTimeout(() => {
       setLoading(false);
+    }, 2000);
+  };
+  const handleSubmitWithServices = service => {
+    setLoadingService({...loadingService, [service]: true});
+    setTimeout(() => {
+      setLoadingService({...loadingService, [service]: false});
     }, 2000);
   };
   return (
@@ -51,15 +62,31 @@ export default function Signin() {
         <MyText customStyles={styles.or}>Or</MyText>
 
         <View style={styles.containerBntServices}>
-          <ButtonServices color={colors.primary} icon="apple" service="Apple" />
+          {Platform.OS === 'ios' && (
+            <ButtonServices
+              loading={loadingService.apple}
+              handlerAction={() => handleSubmitWithServices('apple')}
+              color={colors.primary}
+              icon="apple"
+              service="Apple"
+            />
+          )}
           <View style={{marginVertical: 10}}>
             <ButtonServices
+              loading={loadingService.facebook}
+              handlerAction={() => handleSubmitWithServices('facebook')}
               color={colors.facebook}
               icon="facebook"
               service="Facebook"
             />
           </View>
-          <ButtonServices color="red" icon="google" service="Google" />
+          <ButtonServices
+            loading={loadingService.google}
+            handlerAction={() => handleSubmitWithServices('google')}
+            color="red"
+            icon="google"
+            service="Google"
+          />
         </View>
       </View>
 
