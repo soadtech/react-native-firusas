1c7635be33bfee8b147d740918233ac5b4b8dac7
fernandojoseropero <sdjd7yab4ph4zlsuho5f6j5t4zzagm2iswf72bress3mclqatbgq>
2021-11-18 23:16:26 -0500

    feat: redirecting to login

--- src/screens/Onboarding/index.js
+++ src/screens/Onboarding/index.js
@@ -5,7 +5,11 @@ import {MODEL_INTRO} from '../../assets/images';
 import MyText from '../../commons/MyText';
 import {ButtonRounded} from '../../commons/Buttons';
 
-export default function Onboarding() {
+export default function Onboarding({navigation}) {
+  const handleValidationAuth = () => {
+    // TODO: validate if user is auth
+    navigation.push('signin');
+  };
   return (
     <ImageBackground source={MODEL_INTRO} resiseMode="cover" style={{flex: 1}}>
       <View style={styles.container}>
@@ -14,7 +18,11 @@ export default function Onboarding() {
           A premiun online store for women and their stylish choice.
         </MyText>
         <View style={styles.containerBtn}>
-          <ButtonRounded customStyles={styles.btn} label="Get Started" />
+          <ButtonRounded
+            handlerAction={handleValidationAuth}
+            customStyles={styles.btn}
+            label="Get Started"
+          />
         </View>
       </View>
     </ImageBackground>
