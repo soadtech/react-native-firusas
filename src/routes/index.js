fb24f623136055796d61ed5188d047f015cc55cd
fernandojoseropero <sdjd7yab4ph4zlsuho5f6j5t4zzagm2iswf72bress3mclqatbgq>
2021-11-18 21:47:41 -0500

    feat: creating basic structure to routes

---
+++ src/routes/index.js
@@ -0,0 +1,13 @@
+import Signin from '../screens/Signin';
+import Signup from '../screens/SignUp';
+
+export default [
+  {
+    path: 'signin',
+    component: Signin,
+  },
+  {
+    path: 'signup',
+    component: Signup,
+  },
+];
