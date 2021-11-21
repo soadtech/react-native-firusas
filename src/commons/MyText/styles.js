314ac24f5523868f0b6dde9568095cc8a93fc665
fernandojoseropero <sdjd7yab4ph4zlsuho5f6j5t4zzagm2iswf72bress3mclqatbgq>
2021-11-20 20:49:33 -0500

    style: changing color

--- src/commons/MyText/styles.js
+++ src/commons/MyText/styles.js
@@ -1,9 +1,11 @@
 import {StyleSheet} from 'react-native';
 import fonts from '../../themes/fonts';
+import colors from '../../themes/colors';
 
 export const styles = StyleSheet.create({
   text: {
     ...fonts.regular,
     fontSize: 16,
+    color: colors.primary,
   },
 });
