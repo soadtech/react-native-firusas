c685b87101becd09a015aa0f67d034c090c6dc3f
fernandojoseropero <sdjd7yab4ph4zlsuho5f6j5t4zzagm2iswf72bress3mclqatbgq>
2021-11-20 20:53:47 -0500

    style: chaging color to icons

--- src/components/Home/Header/index.js
+++ src/components/Home/Header/index.js
@@ -3,17 +3,18 @@ import {View} from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 import MyText from '../../../commons/MyText';
 import {styles} from './styles';
+import colors from '../../../themes/colors';
 
 export default function Header() {
   return (
     <View style={styles.container}>
-      <Icon name="search" size={25} />
+      <Icon name="search" size={25} color={colors.primary} />
       <MyText customStyles={styles.title}>Firusas</MyText>
       <View style={styles.containerCart}>
         <View style={styles.cart}>
-          <Icon name="shopping-bag" size={25} />
+          <Icon name="shopping-bag" size={25} color={colors.primary} />
         </View>
-        <Icon name="menu" size={25} />
+        <Icon name="menu" size={25} color={colors.primary} />
       </View>
     </View>
   );
